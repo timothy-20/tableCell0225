d58d3410880e83c9f49357e7b7dd3ba7b38475fe
timothy-20 <jungun3329@gmail.com>
2021-02-25 16:14:14 +0900

    first commit date_0225 time_04.13 커스텀 테이블 뷰 일반 뷰 위에 간단히 구현.

--- tableCell0225/ViewController.m
+++ tableCell0225/ViewController.m
@@ -7,7 +7,8 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UITableViewDelegate, UITableViewDataSource>
+@property (strong, nonatomic) IBOutlet UITableView *myTableView;
 
 @end
 
@@ -15,7 +16,35 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view.
+    
+    self.myTableView = [[UITableView alloc] initWithFrame:CGRectMake(0, 0, self.view.frame.size.width, self.view.frame.size.height) style:UITableViewStylePlain];
+    
+    self.myTableView.delegate = self;
+    self.myTableView.dataSource = self;
+    [self.view addSubview:self.myTableView];
+    //의문 중에 있었던 UITableViewDelegate의 정체. 골자는 위의 3줄.
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    return 3;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"cell"];
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"cell"];
+    }
+    
+    cell.textLabel.text = @"Hello World";
+        
+    return cell;
+}
+
+-(void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    
 }
 
 
